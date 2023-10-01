c767c77300433b1695c6d974d0308a773f2c10e3
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-01 13:44:53 +0200

    Add unit test for validating triggers (#61)

--- packages/data-pusher/src/validation/schema.test.ts
+++ packages/data-pusher/src/validation/schema.test.ts
@@ -1,12 +1,29 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { ZodError } from 'zod';
-import { configSchema, signedApisSchema } from './schema';
+import dotenv from 'dotenv';
+import { Config, configSchema, signedApisSchema } from './schema';
+import { interpolateSecrets } from './utils';
+import { config } from '../../test/fixtures';
 
 it('validates example config', async () => {
-  const config = JSON.parse(readFileSync(join(__dirname, '../../config/pusher.example.json'), 'utf8'));
+  const exampleConfig = JSON.parse(readFileSync(join(__dirname, '../../config/pusher.example.json'), 'utf8'));
 
-  await expect(configSchema.parseAsync(config)).resolves.toEqual(expect.any(Object));
+  // The mnemonic is not interpolated (and thus invalid).
+  await expect(configSchema.parseAsync(exampleConfig)).rejects.toEqual(
+    new ZodError([
+      {
+        code: 'custom',
+        message: 'Invalid mnemonic',
+        path: ['airnodeWalletMnemonic'],
+      },
+    ])
+  );
+
+  const exampleSecrets = dotenv.parse(readFileSync(join(__dirname, '../../config/secrets.example.env'), 'utf8'));
+  await expect(configSchema.parseAsync(interpolateSecrets(exampleConfig, exampleSecrets))).resolves.toEqual(
+    expect.any(Object)
+  );
 });
 
 it('ensures signed API names are unique', () => {
@@ -31,4 +48,25 @@ it('ensures signed API names are unique', () => {
       url: 'https://example.com',
     },
   ]);
+});
+
+it('validates trigger references', async () => {
+  const invalidConfig: Config = {
+    ...config,
+    ois: [
+      // By removing the pre-processing the triggers will end up with different operation effects.
+      { ...config.ois[0]!, endpoints: [{ ...config.ois[0]!.endpoints[0]!, preProcessingSpecifications: undefined }] },
+    ],
+  };
+
+  await expect(configSchema.parseAsync(invalidConfig)).rejects.toEqual(
+    new ZodError([
+      {
+        code: 'custom',
+        message:
+          'If beaconIds contains more than 1 beacon, the endpoint utilized by each beacons must have same operation effect',
+        path: ['triggers', 'signedApiUpdates', 0],
+      },
+    ])
+  );
 });
