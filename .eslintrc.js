534639fa9194e8605b6496541d98098c5cf5071f
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-18 18:27:11 +0200

    Update dependencies (#87)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,7 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.json', './packages/*/tsconfig.json'],
   },
+  rules: {
+    '@typescript-eslint/prefer-destructuring': 'off', // The commons universal ESLint configuration already uses "prefer-destructuring" and this extended version is not needed.
+  },
 };
