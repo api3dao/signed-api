23238793dca065998dc03c6c20cc43c3754612c8
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-29 10:42:53 +0100

    Add stage to signed API

--- packages/pusher/src/validation/schema.ts
+++ packages/pusher/src/validation/schema.ts
@@ -251,6 +251,9 @@ export const nodeSettingsSchema = z.object({
   nodeVersion: z.string().refine((version) => version === packageJson.version, 'Invalid node version'),
   airnodeWalletMnemonic: z.string().refine((mnemonic) => ethers.utils.isValidMnemonic(mnemonic), 'Invalid mnemonic'),
   rateLimiting: rateLimitingSchema,
+  stage: z
+    .string()
+    .regex(/^[\da-z-]{1,16}$/, 'Only lowercase letters, numbers and hyphens are allowed (max 16 characters)'),
 });
 
 export type NodeSettings = z.infer<typeof nodeSettingsSchema>;
