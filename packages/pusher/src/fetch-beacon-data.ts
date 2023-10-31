92815d643a06c67d9b8a8e4bfb6c700bffc1570b
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-31 19:01:41 +0100

    Log the extra time

--- packages/pusher/src/fetch-beacon-data.ts
+++ packages/pusher/src/fetch-beacon-data.ts
@@ -42,11 +42,16 @@ const fetchBeaconDataInLoop = async (signedApiUpdate: SignedApiUpdate) => {
         logger.warn(`Could not put signed response`, { templateId, signedResponse, errorMessage: goPut.error.message });
       }
     });
+
     const duration = Date.now() - startTimestamp;
     // Take at most 10% of the fetch interval as extra time to avoid all API requests be done at the same time. This
     // delay is taken for each interval, so if the system runs for a sufficiently long time, the requests should happen
     // at random intervals.
     const extraTime = Math.random() * signedApiUpdate.fetchInterval * 1000 * 0.1;
+    logger.debug('Adding extra time to fetch interval', {
+      extraTime,
+      fetchInterval: signedApiUpdate.fetchInterval * 1000,
+    });
 
     await sleep(signedApiUpdate.fetchInterval * 1000 - duration + extraTime);
   }
