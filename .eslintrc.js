09513691eddf961bfb399994daf2ff4d4814ec2a
Emanuel Tesař <e.tesarr@gmail.com>
2023-12-04 14:04:15 +0100

    Disable new rule which is too strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/prefer-destructuring': 'off', // The commons universal ESLint configuration already uses "prefer-destructuring" and this extended version is not needed.
+    '@typescript-eslint/max-params': 'off', // It is sometimes necessary to have enough arguments. This rule is too strict.
   },
 };
