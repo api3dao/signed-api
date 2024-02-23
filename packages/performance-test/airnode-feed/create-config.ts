44232f8d2219dce9481ea5f022082be51e0e7321
Emanuel Tesař <e.tesarr@gmail.com>
2024-02-23 07:35:41 +0100

    v0.5.1

--- packages/performance-test/airnode-feed/create-config.ts
+++ packages/performance-test/airnode-feed/create-config.ts
@@ -45,7 +45,7 @@ const configTemplate = {
   ],
   apiCredentials: [],
   nodeSettings: {
-    nodeVersion: '0.5.0',
+    nodeVersion: '0.5.1',
     airnodeWalletMnemonic: 'destroy manual orange pole pioneer enemy detail lady cake bus shed visa',
     stage: 'performance-test',
   },
