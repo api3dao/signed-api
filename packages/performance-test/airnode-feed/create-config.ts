9b3d35d865e5653e5a2694acdbca74adeaa4481e
bdrhn9 <bedirhan@api3.org>
2025-02-17 13:01:07 +0300

    v3.0.0

--- packages/performance-test/airnode-feed/create-config.ts
+++ packages/performance-test/airnode-feed/create-config.ts
@@ -45,7 +45,7 @@ const configTemplate = {
   ],
   apiCredentials: [],
   nodeSettings: {
-    nodeVersion: '2.1.0',
+    nodeVersion: '3.0.0',
     airnodeWalletMnemonic: 'destroy manual orange pole pioneer enemy detail lady cake bus shed visa',
     stage: 'performance-test',
   },
